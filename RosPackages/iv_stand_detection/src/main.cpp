864aabc955e0a02ff180d85931216e115518795e
Mrinaank Sinha <mrinaanks@gmail.com>
2022-01-27 19:00:32 +1100

    Added callback for lase scan

--- RosPackages/iv_stand_detection/src/main.cpp
+++ RosPackages/iv_stand_detection/src/main.cpp
@@ -3,10 +3,21 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
+cv::Mat curFrame;
+
 void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
     try {
         cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-        cv::waitKey(30);
+        cv::waitKey(0);
+    }
+    catch (cv_bridge::Exception& e) {
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
+}
+
+void scanCallback(const sensor_msgs::LaserScanCal::ConstPtr& scan) {
+    try {
+        std::cout << scan.intensities.length() << std::endl;
     }
     catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
@@ -14,12 +25,16 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "image_listener");
+    ros::init(argc, argv, "offsetCalculator");
     ros::NodeHandle nh;
-    cv::namedWindow("view");
 
+    cv::namedWindow("view");
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("/simon/camera_base_mount/simon/image_raw", 1, imageCallback);
+
+    ros::Subscriber sub = nh.subscribe("/scan", 1000, scanCallback);
+
+
     ROS_INFO("%s", sub.getTopic().c_str());
     ros::spin();
     cv::destroyWindow("view");
