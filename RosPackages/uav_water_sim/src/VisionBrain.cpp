fb88de52ab05dce5f7fad81c199c785b19b0d8aa
Mrinaank Sinha <mrinaanks@gmail.com>
2022-02-13 00:31:11 +1100

    Added movement publishing

--- RosPackages/uav_water_sim/src/VisionBrain.cpp
+++ RosPackages/uav_water_sim/src/VisionBrain.cpp
@@ -9,11 +9,9 @@ VisionBrain::VisionBrain() {
     windowName = cv::String("view");
     cv::namedWindow(windowName);
 
-    image_transport::ImageTransport it{ nh };
-    imageSub = it.subscribe("/usb_cam/image_raw", 1, &VisionBrain::imageRecievedCallback, this);
+    imageSub = nh.subscribe("iris/usb_cam/image_raw", 1, &VisionBrain::imageRecievedCallback, this);
 
-    pumpPub = nh.advertise<std_msgs::Bool>("/uav/pump/activate", 1);
-    pumpSub = nh.subscribe("/uav/pump/isActive", 1, &VisionBrain::pumpIsActiveCallback, this);
+    movePub = nh.advertise<std_msgs::Bool>("/move", 1);
 }
 
 VisionBrain::~VisionBrain() {
@@ -23,15 +21,65 @@ VisionBrain::~VisionBrain() {
 void VisionBrain::imageRecievedCallback(const sensor_msgs::ImageConstPtr& msg) {
     try {
         curFrame = cv_bridge::toCvShare(msg, "bgr8")->image;
-
     }
     catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     }
 }
 
-void VisionBrain::pumpIsActiveCallback(const std_msgs::Bool::ConstPtr& pumpFinished) {
-    isPumpActive = pumpFinished->data;
+void VisionBrain::move(int forwardBackward, int leftRight, int upDown, int yawLeftRight, int actuatorOpen) {
+    std_msgs::Float64MultiArray result;
+
+    auto h{ 1 }, w{ 5 };
+
+    result.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    result.layout.dim[0].label = "height";
+    result.layout.dim[0].size = h;
+    result.layout.dim[0].stride = h * w;
+
+    result.layout.dim.push_back(std_msgs::MultiArrayDimension());
+    result.layout.dim[0].label = "width";
+    result.layout.dim[0].size = w;
+    result.layout.dim[0].stride = w;
+
+    result.layout.data_offset = 0;
+
+    std::vector<double> vec;
+
+    std::string result = "Command: ";
+    if (forwardBackward != 0) {
+        result += forwardBackward == 1 ? "FORWARD" : "BACKWARD";
+        result += " ";
+    }
+    if (leftRight != 0) {
+        result += leftRight == 1 ? "LEFT" : "RIGHT";
+        result += " ";
+    }
+    if (upDown != 0) {
+        result += upDown == 1 ? "UP" : "DOWN";
+        result += " ";
+    }
+    if (yawLeftRight != 0) {
+        result += yawLeftRight == 1 ? "YAW_LEFT" : "YAW_RIGHT";
+        result += " ";
+    }
+    if (actuatorOpen != 0) {
+        result += actuatorOpen == 1 ? "ActuatorOpen" : "ActuatorClose";
+        result += " ";
+    }
+    if (forwardBackward == 0 && leftRight == 0 && upDown == 0 && yawLeftRight == 0 && actuatorOpen == 0) {
+        result += "STOP";
+        result += " ";
+    }
+
+    vec.push_back(forwardBackward);         // Forward (1)/Back (-1)
+    vec.push_back(leftRight);               // Left (1)/Right (-1)
+    vec.push_back(upDown);                  // Up (1)/ Down (-1)
+    vec.push_back(yawLeftRight);            // Yaw left (1)/ Yaw Right (-1)
+    vec.push_back(actuatorOpen);            // Acuator Open (1)/Acuator Close (-1)
+
+    result.data = vec;
+    movePub.publish(result);
 }
 
 bool VisionBrain::executeTasks() {
@@ -84,62 +132,57 @@ bool VisionBrain::executeTasks() {
 
 
     cv::imshow(windowName, curFrame);
-    cv::waitKey(25);
+    cv::waitKey(1);
     return true;
 }
 
-void VisionBrain::activatePump(bool activate) {
-    std_msgs::Bool result;
-    result.data = activate;
-
-    pumpPub.publish(result);
-}
 
 void VisionBrain::takeoff() {
     if (counter < 20) {
-        printf("\nCommand: UP");
+        move(0, 0, 1, 0, 0);
         counter++;
         return;
     }
 
     taskNumber++;
-    printf("\nCommand: STOP");
+    move(0, 0, 0, 0, 0);
 }
 
 void VisionBrain::printInstruction(cv::Point locationOffset) {
-    std::string horizontal;
-    std::string vertical;
+    int horizontal;
+    int vertical;
 
     if (locationOffset == cv::Point(0, 0)) {
-        printf("\nCommand: STOP!");
+        move(0, 0, 0, 0, 0);
     }
 
     if (locationOffset.x < 0) {
-        horizontal = "LEFT ";
+        horizontal = 1;
     } else if (locationOffset.x > 0) {
-        horizontal = "RIGHT ";
+        horizontal = -1;
     }
 
     if (locationOffset.y < 0) {
-        vertical = "FORWARD ";
+        vertical = 1;
     } else if (locationOffset.y > 0) {
-        vertical = "BACKWARD ";
+        vertical = -1;
     }
 
     printf("\nCommand: %s %d, %s, %d", horizontal.c_str(), abs(locationOffset.x), vertical.c_str(), abs(locationOffset.y));
+    move(vertical, horizontal, 0, 0, 0);
 }
 
 void VisionBrain::findPool() {
 
     auto resultList = PoolDetector::getPoolOffset(curFrame);
     if (resultList.size() == 0) {
-        printf("\nCommand: LEFT");
+        move(1, 0, 0, 0, 0);
         return;
     }
     auto result = resultList[0];
 
     if (abs(result.x) <= 80 && abs(result.y) <= 80) {
-        printf("\nCommand: STOP");
+        move(0, 0, 0, 0, 0);
         taskNumber++;
     } else {
         printInstruction(result);
@@ -148,12 +191,12 @@ void VisionBrain::findPool() {
 
 void VisionBrain::descendAboveZone() {
     if (counter < 15) {
-        printf("\nCommand: DOWN");
+        move(0, 0, -1, 0, 0);
         counter++;
         return;
     }
 
-    printf("\nCommand: STOP");
+    move(0, 0, 0, 0, 0);
     activatePump(true);
     isPumpActive = true;
     taskNumber++;
@@ -171,7 +214,7 @@ void VisionBrain::waitForWaterCollection() {
 void VisionBrain::findCheckerBoard() {
     auto resultList = CheckerboardDetector::getCheckerboardLocation(curFrame);
     if (resultList.size() == 0) {
-        printf("\nCommand: LEFT");
+        move(0, 1, 0, 0, 0);
         return;
     }
     auto result = result[0];
@@ -179,7 +222,7 @@ void VisionBrain::findCheckerBoard() {
     result = cv::Point((int)curFrame.cols / 2, (int)curFrame.rows / 2) - result;
 
     if (abs(result.x) <= 30 && abs(result.y) <= 30) {
-        printf("\nCommand: STOP");
+        move(0, 0, 0, 0, 0);
         taskNumber++;
     } else {
         printInstruction(result);
@@ -190,17 +233,17 @@ void VisionBrain::findCheckerBoard() {
 
 void VisionBrain::returnToHome() {
     if (counter < 15) {
-        printf("\nCommand: LEFT");
+        move(0, 1, 0, 0, 0);
         counter++;
         return;
     }
 
     if (counter < 30) {
-        printf("\nCommand: DOWN");
+        move(0, 0, -1, 0, 0);
         counter++;
         return;
     }
-    printf("\nCommand: STOP");
+    move(0, 0, 0, 0, 0);
     taskNumber++;
     return;
 }
